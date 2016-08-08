046208841e6b3f96cd2fa15e5f21b78ec8ed6c5d
SoleUce <ftcube64@gmail.com>
2016-08-08 22:52:29 +1200

    added reply feature

--- bot.js
+++ bot.js
@@ -4,16 +4,12 @@ var Twit = require('twit');
 
 var config = require('./config');
 
-
-
 var T = new Twit(config);
-
-
-tweetIt();
-setInterval(tweetIt, 1000 * 86400)
 
 tweetPic();
 setInterval(tweetPic, 1000 * 86400)
+
+
 
 //Follow Back
 var stream = T.stream('user');
@@ -22,52 +18,65 @@ var stream = T.stream('user');
 stream.on('tweet', tweetEvent);
 
 function tweetEvent(eventMsg) {
-	var fs = require('fs');
-	var json = JSON.stringify(eventMsg,null,2);
-	fs.writeFile("tweet.json", json);
+	//var fs = require('fs');
+	//var json = JSON.stringify(eventMsg,null,2);
+	//fs.writeFile("tweet.json", json);
+
+	var replyto = eventMsg.in_reply_to_screen_name;
+	var	event = eventMsg.text;
+	var from = eventMsg.user.screen_name;
+
+	console.log(replyto + ' ' + from);
+
+	if (replyto == 'AnimeBagelBot') {
+		var newtweet = "Thank You " + '@' + from + " have a Bagel"
+		tweetIt(newtweet);
+	}
 
 }
 
+//
+//Tweet A message
+//
 function tweetIt(txt) {
 
 	var tweet = {
 		status: txt
 	}
 
-
-
+	T.post('statuses/update', tweet) 
+	
 }
 
-//Post Random Number
-}
 
 //
 // post a pic
 //
 
 function tweetPic() {
-var r = Math.floor(Math.random()*20) + 1;
-var fileName ='Pics/' + r +'.png'
-var fs = require('fs')
-var b64content = fs.readFileSync(fileName, { encoding: 'base64' })
+	var r = Math.floor(Math.random()*20) + 1;
+	var fileName ='Pics/' + r +'.png'
+	var fs = require('fs')
+	var b64content = fs.readFileSync(fileName, { encoding: 'base64' }) 
 
-// first we must post the media to Twitter
-T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-  // now we can assign alt text to the media, for use by screen readers and
-  // other text-based presentations and interpreters
-  var mediaIdStr = data.media_id_string
-  var altText = "Nice Bagel M8."
-  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+	// first we must post the media to Twitter
+	T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+	  // now we can assign alt text to the media, for use by screen readers and
+	  // other text-based presentations and interpreters
+	  var mediaIdStr = data.media_id_string
+	  var altText = "Nice Bagel M8."
+	  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
-  T.post('media/metadata/create', meta_params, function (err, data, response) {
-    if (!err) {
-      // now we can reference the media and post a tweet (media will attach to the tweet)
-      var params = { status: 'Bagels brought to you by #SolesBagelBot', media_ids: [mediaIdStr] }
+	  T.post('media/metadata/create', meta_params, function (err, data, response) {
+	    if (!err) {
+	      // now we can reference the media and post a tweet (media will attach to the tweet)
+	      var params = { status: 'Bagels brought to you by #SolesBagelBot', media_ids: [mediaIdStr] }
 
-      T.post('statuses/update', params, function (err, data, response) {
-        console.log(data)
-      })
-    }
-  })
-})
+	      T.post('statuses/update', params, function (err, data, response) {
+	        console.log(data)
+	      })
+	    }
+	  })
+	})
 }
+
